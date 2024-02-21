1341932457f325e4f40f1087634a2da62d37579a
PandaExpress053 <ethan.fung25@ycdsbk12.ca>
2024-02-21 18:03:24 -0500

    modified:   Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,52 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
   /**
-   * Called once at the beginning of execution, put your size all in this method
+   * Creates a picture of a house, grass, and a sun
+   * and resizes to desired image size
+   * @author: E.Fung
    */
-  public void settings() {
-	// put your size call here
-    size(400, 400);
-  }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
   
-  // define other methods down here.
+	// Initialize desired size of image and the original size of image
+	double dblNewSizeX = 600;
+  double dblNewSizeY = 600;
+  double dblOriginalSizeX = 400;
+  double dblOriginalSizeY = 400;
+  double dblMultiplierX;
+  double dblMultiplierY;
+
+  public void settings() {
+	  // put your size call here
+    size((int) dblNewSizeX, (int) dblNewSizeY);
+    
+    //Calculate Multiplier/Scaling Value
+    dblMultiplierX = dblNewSizeX / dblOriginalSizeX;
+    dblMultiplierY = dblNewSizeY / dblOriginalSizeY;
+  }
+
+  public void setup() {
+    // Sets blue background
+    background(0, 0, 255);
+  }
+
+  public void draw() {
+    // Creating green foreground (with scaling factor)
+    fill(0, 255, 0);
+    rect((int) (0 * dblMultiplierX), (int) (300 * dblMultiplierY), (int) (400 * dblMultiplierX), (int) (400 * dblMultiplierY));
+
+    // Creating gray square house base (with scaling factor)
+    fill(150, 150, 150);
+    rect((int) (150 * dblMultiplierX), (int) (335 * dblMultiplierY), (int) (200 * dblMultiplierX), (int) (-200 * dblMultiplierY));
+
+    // Creating white triangle roof (with scaling factor)
+    strokeWeight(2);
+    fill(255, 255, 255);
+    triangle((int) (150 * dblMultiplierX), (int) (135 * dblMultiplierY), (int) (250 * dblMultiplierX), (int) (80 * dblMultiplierY), (int) (350 * dblMultiplierX), (int) (135 * dblMultiplierY));
+    strokeWeight(1);
+
+    // Creating circular yellow sun in top left corner (with scaling factor)
+    fill(255, 255, 150);
+    ellipse((int) (25 * dblMultiplierX), (int) (25 * dblMultiplierY), (int) (150 * dblMultiplierX), (int) (150 * dblMultiplierY));
+  }
+
 }
